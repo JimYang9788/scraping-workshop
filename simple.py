5da51e61c9e0851f0f1e332200ea22e3bd75d485
Jim Yang <yang9788@yahoo.com>
2023-01-02 17:02:58 -0500

    adding resp object

--- simple.py
+++ simple.py
@@ -1,8 +1,10 @@
-from urllib.request import urlopen 
-
+from urllib.request import urlopen ## Gives you url 
+import requests  # gives you a response object
 def main():
+    URL = "http://pythonscraping.com/pages/page1.html"
     html = urlopen('http://pythonscraping.com/pages/page1.html')
     print (html)
+    import pdb; pdb.set_trace()
     print ("Running scraper...")
 
 
